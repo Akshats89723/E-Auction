7c3cd088b4a2b33674797370aad4f68bed3172a7
Akshats89723 <sanghviakshat7@gmail.com>
2026-08-27 14:42:31 +0530

    Fix Daphne AppRegistryNotReady exception by initializing get_asgi_application early

--- core/asgi.py
+++ core/asgi.py
@@ -8,16 +8,19 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/asgi/
 """
 
 import os
-
 from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+
+# Initialize Django ASGI application early to ensure the AppRegistry is populated
+django_asgi_app = get_asgi_application()
+
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 import auctions.routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
-
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
+    "http": django_asgi_app,
     "websocket": AuthMiddlewareStack(
         URLRouter(
             auctions.routing.websocket_urlpatterns
